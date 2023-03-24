bacbced28cb026100c9c571ed375ce31332e8097
Tuna <df95111111@gmail.com>
2023-03-24 21:19:18 +0800

    Add generateRandomChinese function to generate random Chinese characters in index.js and add tests for it in test.js

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,4 +1,7 @@
-const { generateRandomString } = require('../index');
+const {
+  generateRandomString,
+  generateRandomChinese,
+} = require('../index');
 
 describe('generateRandomString', () => {
   test('returns a string of the correct length', () => {
@@ -7,6 +10,17 @@ describe('generateRandomString', () => {
   test('returns a different string each time', () => {
     const str1 = generateRandomString(10);
     const str2 = generateRandomString(10);
+    expect(str1).not.toBe(str2);
+  });
+});
+
+describe('generateRandomChinese', () => {
+  test('returns a string of the correct length', () => {
+    expect(generateRandomChinese(10).length).toBe(10);
+  });
+  test('returns a different string each time', () => {
+    const str1 = generateRandomChinese(10);
+    const str2 = generateRandomChinese(10);
     expect(str1).not.toBe(str2);
   });
 });
